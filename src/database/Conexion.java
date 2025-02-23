d97c4ea0a2e515e14166a7b26d5e535f3020bd7c
Elizabeth Abrego <an0174032023@unab.edu.sv>
2025-02-22 18:28:46 -0600

    Guías desde la 1: 'Creación de un proyecto en java' hasta la 8: 'Patrón DAO', finalizadas.

---
+++ src/database/Conexion.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/*---------------------------------
+/**
+ *
+ * @author Elizabeth
+ */
+
+
+public class Conexion {
+    private final String DB_DRIVER = "com.mysql.jdbc.Driver";
+    private final String URL="jdbc:mysql://localhost:3306/";  
+    private final String DB = "puntoventa2";
+    private final String USER = "root";
+    private final String PASSWORD = "1234"; 
+    
+    Connection connection;
+    
+    public static Conexion singleConnection;
+    
+    private Conexion(){
+        this.connection = null;
+    }
+    
+    public Connection conectar(){
+        try{
+           Class.forName(DB_DRIVER);
+           this.connection = DriverManager.getConnection(
+           URL + DB, USER, PASSWORD
+           );
+        }
+        catch(ClassNotFoundException | SQLException e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            System.exit(0);
+        }
+        return this.connection;
+    }
+    
+    public void desconectar(){
+        try{
+            this.connection.close();
+        }
+        catch(SQLException e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+    
+    public synchronized static Conexion getInstance(){
+        if(singleConnection == null){
+            singleConnection = new Conexion();
+        }
+        return singleConnection;
+    }
+    
+    
+}
+
